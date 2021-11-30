0665ae168ef70efeacad53dbcc1d479b04350d5a
wgm <konmyn@gamil.com>
2021-11-30 23:51:28 +0800

    kotlin 第一课

---
+++ TypeInfer.kt
@@ -0,0 +1,19 @@
+//类型推断
+fun main(args:Array<String>) {
+    var i = 18
+    i = 19
+    var j = 999999999999999999
+    //显式类型声明
+    var k:Long = 999999999999999999
+    var s:String = "haha"
+    println(i)
+    println(j)
+    println(k)
+    println(s)
+
+    var a:Int
+    var b:Short
+    //只读数据，不可修改
+    val number = "No.123456"
+
+}
---
+++ VarAndVal.kt
@@ -0,0 +1,7 @@
+//变量声明和使用
+
+fun main(args:Array<String>) {
+    var name = "张三"
+    name = "李四"
+    println(name)
+}
